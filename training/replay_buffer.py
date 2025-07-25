4ea0b327b6faeac9888d715baa67635621890a5a
、、 <hopingzhang@foxmail.com>
2025-07-25 08:19:32 +0800

    ——

--- training/replay_buffer.py
+++ training/replay_buffer.py
@@ -1,4 +1,5 @@
 import random
+import threading
 from collections import deque
 from copy import deepcopy
 
@@ -6,6 +7,7 @@ from copy import deepcopy
 class ReplayBuffer:
     def __init__(self, capacity):
         self.buffer = deque(maxlen=capacity)
+        self.lock = threading.Lock()
 
     def add_transition(self, state, due_time_of_order_, enable_transition_idxs, action, action_prob, advantage):
         transition = {
@@ -16,9 +18,12 @@ class ReplayBuffer:
             'action_prob': action_prob,
             'advantage': advantage,
         }
-        self.buffer.append(transition)
+        with self.lock:
+            self.buffer.append(transition)
 
     def sample_batch(self, batch_size):
         """采样"""
-        batch = random.sample(self.buffer, batch_size)
+        with self.lock:
+            batch = random.sample(self.buffer, batch_size)
         return batch
+
